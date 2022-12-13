615aab97d254bc56515f78f584eed524ec5e823d
abirtasrif <abirtasrif@gmail.com>
2022-12-14 00:24:45 +0600

    starting project

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return 
+  <nav className="navbar wrapper">
+    <ul className="links">
+      <li><Link></Link></li>
+    </ul>
+    <div className="logo"></div>
+    <div className="favourite-link"></div>
+  </nav>
+};
+
+export default Navbar;
