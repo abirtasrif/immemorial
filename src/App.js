643c6d7284117c3f0bca9e15f4d588776cec31d6
abirtasrif <abirtasrif@gmail.com>
2022-12-14 20:05:42 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return 
+  return (
   <nav className="navbar wrapper">
     <ul className="links">
-      <li><Link></Link></li>
+      <li><Link to="/featured">featured</Link></li>
+      <li><Link to="/about">about</Link></li>
+      <li><Link to="/gallery">gallery</Link></li>
     </ul>
-    <div className="logo"></div>
-    <div className="favourite-link"></div>
+    <div className="logo"><Link to="/">immemorial</Link></div>
+    <div className="favourite-link"><Link to="/">favourite</Link></div>
   </nav>
+  )
 };
 
 export default Navbar;
