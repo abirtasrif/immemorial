2cab1f38f1b0a65d38075554f313a1ae9a550ec0
abirtasrif <abirtasrif@gmail.com>
2022-12-18 17:15:42 +0600

    adding production

---
+++ build/precache-manifest.f5397cdd5a453fcab74ddae612f4aa73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f65999ad2f8503d6b281",
+    "url": "/static/css/main.86ab8617.chunk.css"
+  },
+  {
+    "revision": "f65999ad2f8503d6b281",
+    "url": "/static/js/main.f65999ad.chunk.js"
+  },
+  {
+    "revision": "bc3becbfabb43fd7b183",
+    "url": "/static/js/1.bc3becbf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c49049501a44a334de374f68e6693a5",
+    "url": "/index.html"
+  }
+];
